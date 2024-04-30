307e347bc1aede5fd4808a32952d109f635d87af
EricWithC04 <negativegame117@gmail.com>
2024-04-29 21:43:31 -0300

    feat: function convertToTensor

--- index.js
+++ index.js
@@ -41,3 +41,32 @@ function createModel() {
 
 const model = createModel()
 tfvis.show.modelSummary({name: 'Model Summary'}, model)
+
+function convertToTensor(data) {
+    return tf.tify(() => {
+        tf.util.shuffle(data);
+
+        const inputs = data.map(d => d.horsepower)
+        const labels = data.map(d => d.mpg)
+
+        const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+        const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+        const inputMax = inputTensor.max();
+        const inputMin = inputTensor.min();
+        const labelMax = labelTensor.max();
+        const labelMin = labelTensor.min();
+
+        const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+        const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+
+        return {
+            inputs: normalizedInputs,
+            labels: normalizedLabels,
+            inputMax,
+            inputMin,
+            labelMax,
+            labelMin
+        }
+    })
+}
