2953fa4bff220170e58913f0975fb77a4f4a1b5b
EricWithC04 <negativegame117@gmail.com>
2024-04-29 21:06:09 -0300

    feat: fetch to google api

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+async function getData () {
+    const carsDataResponse = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
+    const carsData = await carsDataResponse.json();
+    const cleaned = carsData.map(car => ({
+        mpg: car.Miles_per_Gallon,
+        horsepower: car.Horsepower
+    }))
+    .filter(car => (car.mpg !== null && car.horsepower !== null));
+
+    return cleaned
+}
+
+console.log(getData());
